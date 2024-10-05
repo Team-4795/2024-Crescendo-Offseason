16a3dfcbf8a5a88d396779541b8e2cf0cf97e932
Arenacloserr <162641455+Arenacloserr@users.noreply.github.com>
2024-10-04 23:15:38 -0400

    sim update

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,3 +1,20 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeIOSim {}
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class IntakeIOSim implements IntakeIO {
+  private final DCMotorSim motor = new DCMotorSim(DCMotor.getNeoVortex(1), 1, 1);
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    motor.update(0.01);
+
+    inputs.Amps = motor.getCurrentDrawAmps();
+  }
+
+  @Override
+  public void setMotorSpeed(double speed) {
+    motor.setInputVoltage(speed);
+  }
+}
